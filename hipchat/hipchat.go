48346f38a08f87cfad480268ed907e6a0f291605
Adam Bachman <adam.bachman@gmail.com>
2014-11-07 11:45:07 -0500

    move hipchat message definition into hipchat.go

--- hipchat/hipchat.go
+++ hipchat/hipchat.go
@@ -108,3 +108,34 @@ func (m *message) ChannelName() string {
 func (m *message) Text() string {
 	return m.text
 }
+// hipchat webhook API
+type User struct {
+	ID          int    `json:"id"`
+	MentionName string `json:"mention_name"`
+	Name        string `json:"name"`
+}
+
+type Message struct {
+	ID      string `json:"id,omitempty"`
+	Date    string `json:"date,omitempty"`
+	Message string `json:"message,omitempty"`
+	From    User   `json:"from,omitempty"`
+}
+
+type Room struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type MessageData struct {
+	Message Message `json:"message"`
+	Room    Room    `json:"room"`
+}
+
+// top level hipchat webhook JSON object
+type WebhookMessage struct {
+	Event         string      `json:"string"`
+	Item          MessageData `json:"item"`
+	WebhookID     int         `json:"webhook_id"`
+	OauthClientID string      `json:"oauth_client_id"`
+}
